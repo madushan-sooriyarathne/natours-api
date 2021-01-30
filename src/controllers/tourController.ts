c6e8d07c7f6057d6e36a9ad63a398bfadecbe5a9
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-30 10:50:41 +0530

    refactored monoose logic

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -1,54 +1,149 @@
 import { NextFunction, Request, Response } from "express";
 
-import { controller, del, get, patch, post, put } from "./decorators";
-import { use } from "./decorators/use";
+import {
+  controller,
+  del,
+  get,
+  patch,
+  post,
+  put,
+  use,
+  validateBody,
+} from "./decorators";
+import Tour from "../models/Tour";
+import { TypeStrings } from "./decorators/enums/typeStrings";
+import { Query } from "mongoose";
+import APIOperations from "../utils/APIOperations";
 
-function validateId(req: Request, res: Response, next: NextFunction): void {
-  if (parseInt(req.params.id) === 20) {
-    res.status(400).json({ status: "failed", data: null });
+async function validateId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  if (!req.params.id) {
+    res.status(400).json({ status: "failed", message: "invalid request" });
     return;
   }
-  next();
+
+  try {
+    const fetchedTour = await Tour.findById(req.params.id);
+    console.log(req.params.id);
+    console.log(fetchedTour);
+    if (!fetchedTour) {
+      res.status(404).json({ status: "failed", message: "invalid tour id" });
+      return;
+    }
+    next();
+  } catch (error: any) {
+    res.status(400).json({ status: "failed", message: "invalid request" });
+  }
 }
 
 @controller("/api/v1/tours")
 class TourController {
   @get("/")
-  getTours(req: Request, res: Response): void {
-    res.send("you got the tours");
+  async getTours(req: Request, res: Response): Promise<void> {
+    try {
+      const apiOperationQuery = new APIOperations<TourDocument, TourDocument>(
+        Tour.find(),
+        req.query
+      );
+
+      // build the result
+      const tours: TourDocument[] = await apiOperationQuery
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate().query;
+      console.log(tours);
+
+      res
+        .status(200)
+        .json({ status: "success", count: tours.length, data: tours });
+    } catch (error: any) {
+      res.status(404).json({ status: "failed", message: error.message });
+    }
   }
 
   @post("/")
-  addTour(req: Request, res: Response): void {
-    res.send("you added a tour");
+  @validateBody(
+    { name: "name", type: TypeStrings.String },
+    { name: "price", type: TypeStrings.Number },
+    { name: "difficulty", type: TypeStrings.String },
+    { name: "duration", type: TypeStrings.Number }
+  )
+  async addTour(req: Request, res: Response): Promise<void> {
+    try {
+      const newTour = await Tour.create({
+        name: req.body.name,
+        price: req.body.price,
+        rating: req.body.rating,
+        difficulty: req.body.difficulty,
+        duration: req.body.duration,
+        maxGroupSize: req.body.maxGroupSize,
+      });
+
+      res.status(201).json({ status: "success", data: newTour });
+    } catch (error: any) {
+      res.status(500).json({ status: "failed", message: error.message });
+    }
   }
 
   @patch("/:id")
   @use(validateId)
-  editTour(req: Request, res: Response): void {
-    const id: number = parseInt(req.params.id);
+  async editTour(req: Request, res: Response): Promise<void> {
+    try {
+      const updatedTour = await Tour.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true, runValidators: true }
+      );
 
-    // Tour Object edit login here
-
-    res.send("Entry edited successfully");
+      res.status(200).json({ status: "success", data: updatedTour });
+    } catch (error: any) {
+      res.status(500).json({ status: "failed", message: error.message });
+    }
   }
 
   @put("/:id")
   @use(validateId)
-  updateTour(req: Request, res: Response): void {
-    const id: number = parseInt(req.params.id);
+  @validateBody(
+    { name: "name", type: TypeStrings.String },
+    { name: "price", type: TypeStrings.Number },
+    { name: "difficulty", type: TypeStrings.String },
+    { name: "duration", type: TypeStrings.Number }
+  )
+  async updateTour(req: Request, res: Response): Promise<void> {
+    try {
+      const changedTour = await Tour.findByIdAndUpdate(
+        req.params.id,
+        {
+          name: req.body.name,
+          price: req.body.price,
+          rating: req.body.rating,
+          difficulty: req.body.difficulty,
+          duration: req.body.duration,
+          maxGroupSize: req.body.maxGroupSize,
+        },
+        { new: true, runValidators: true }
+      );
 
-    // tour entry update logic here
-
-    res.send("Tour updated successfully");
+      res.status(200).json({ status: "success", data: changedTour });
+    } catch (error: any) {
+      res.status(500).json({ status: "failed", message: error.message });
+    }
   }
 
   @del("/:id")
   @use(validateId)
-  deleteTour(req: Request, res: Response): void {
-    const id: number = parseInt(req.params.id);
+  async deleteTour(req: Request, res: Response): Promise<void> {
+    try {
+      await Tour.findByIdAndDelete(req.params.id);
 
-    // Tour Entry delete logic here
+      res.status(204).json({ status: "success", data: null });
+    } catch (error: any) {
+      res.status(200).json({ status: "failed", message: error.message });
+    }
 
     res.status(204).json({ status: "success", data: null });
   }

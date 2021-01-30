596c604b67513b40ca4972c556c9e5a130ba8410
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-30 10:53:50 +0530

    created new enum typeStrings

---
+++ src/controllers/decorators/enums/typeStrings.ts
@@ -0,0 +1,11 @@
+export enum TypeStrings {
+  Undefined = "undefined",
+  Null = "object",
+  Object = "object",
+  Boolean = "boolean",
+  Number = "number",
+  BigInt = "bigint",
+  String = "string",
+  symbol = "symbol",
+  Function = "function",
+}

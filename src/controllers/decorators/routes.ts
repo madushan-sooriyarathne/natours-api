2267cb4421d55342ea8caa8667c9e4b668c8f326
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-03 02:02:47 +0530

    added all route

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -16,3 +16,4 @@ export const post = routeBinder(Methods.post);
 export const put = routeBinder(Methods.put);
 export const del = routeBinder(Methods.del);
 export const patch = routeBinder(Methods.patch);
+export const all = routeBinder(Methods.all);

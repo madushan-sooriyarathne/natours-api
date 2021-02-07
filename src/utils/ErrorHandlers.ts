da8b079b7204f5542c57b12d970d767db320f465
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-07 23:58:00 +0530

    added multiple custom error response types

--- src/utils/ErrorHandlers.ts
+++ src/utils/ErrorHandlers.ts
@@ -2,31 +2,55 @@ import { Request, Response, NextFunction } from "express";
 import AppError from "./AppError";
 
 /**
- * Error request handler for MongoError 11000 (Duplicate Key)
+ * Error request handler for Mongodb DuplicateKeyError
  * @param {Error} err - Error object
- * @param {Express.Response} res - Express's HTTP Response object
+ * @returns {AppError} - AppError instance with message and proper http error details
  */
-async function handleDuplicateKeyError(
-  err: Error,
-  res: Response
-): Promise<void> {
+function handleDuplicateKeyError(err: Error): AppError {
   const [key, value]: [string, unknown] = Object.entries(
     (err as { [key: string]: any }).keyValue
   )[0];
 
-  res.status(409).json({
-    status: "failed",
-    message: `Duplicate Key - another object with value '${value}' as it's ${key} field already exists!`,
-  });
+  return new AppError(
+    `Duplicate Key - another object with value '${value}' as it's ${key} field already exists!`,
+    409,
+    "failed"
+  );
 }
 
 /**
  * Error request handler for Mongodb ValidatorError
  * @param {Error} err - Error object
- * @param {Express.Response} res - Express's HTTP Response object
+ * @returns {AppError} - AppError instance with message and proper http error details
  */
-async function handleValidatorError(err: Error, res: Response): Promise<void> {
-  res.status(406).json({ status: "failed", message: err.message });
+function handleValidatorError(err: Error): AppError {
+  return new AppError(err.message, 406, "failed");
+}
+
+/**
+ * Error request handler for JWT expired error
+ * @param {Error} err - Error object
+ * @returns {AppError} - AppError instance with message and proper http error details
+ */
+function handleJWTExpiredError(err: Error): AppError {
+  return new AppError(
+    "Auth Token is expired! Please log-in again",
+    401,
+    "failed"
+  );
+}
+
+/**
+ * Error request handler for Mongodb ValidatorError
+ * @param {Error} err - Error object
+ * @returns {AppError} - AppError instance with message and proper http error details
+ */
+function handleJWTTamperedError(err: Error): AppError {
+  return new AppError(
+    "Auth Token cannot be validated. Please log-in again",
+    401,
+    "failed"
+  );
 }
 
 /**
@@ -34,12 +58,12 @@ async function handleValidatorError(err: Error, res: Response): Promise<void> {
  * @param {Error} err - Error object
  * @param {Express.Response} res - Express's HTTP Response object
  */
-async function globalErrorHandler(
+function globalErrorHandler(
   err: AppError,
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void> {
+): void {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "failed";
 
@@ -49,32 +73,42 @@ async function globalErrorHandler(
       .status(err.statusCode)
       .json({ status: err.status, message: err.message, error: err });
   } else if (process.env.NODE_ENV === "production") {
-    // Filter error message with if error is not a operational error
+    // Special Error types and handlers
+    // MongoError (duplicate key error)
+    if ((err as { [key: string]: any }).code === 11000)
+      err = handleDuplicateKeyError(err);
+
+    // MongoDB ValidatorError (mongoose validation is failed)
+    if ((err as { [key: string]: any }).errors) {
+      const val: any[] = Object.values((err as { [key: string]: any }).errors);
+
+      if (val.length > 0 && val[0].name === "ValidatorError") {
+        err = handleValidatorError(err);
+      }
+    }
+
+    // JWT expired
+    if ((err as { [key: string]: any }).name == "TokenExpiredError") {
+      err = handleJWTExpiredError(err);
+    }
+
+    // JWT cannot verified
+    if ((err as { [key: string]: any }).name == "JsonWebTokenError") {
+      err = handleJWTTamperedError(err);
+    }
+
+    // Filter error message with error type (AppError or not - using the opError field in possible AppError objects)
     if (err.isOpError) {
-      // if isOpError is not null, means this is an AppError instance
+      // if isOpError is true, means this is an AppError instance
       res
         .status(err.statusCode)
         .json({ status: err.status, message: err.message });
       return;
     }
 
-    if ((err as { [key: string]: any }).code === 11000) {
-      // MongoError (duplicate key error)
-      handleDuplicateKeyError(err, res);
-      return;
-    }
-
-    const [_, value]: [string, any] = Object.entries(
-      (err as { [key: string]: any }).errors
-    )[0];
-
-    if (value.name === "ValidatorError") {
-      // Mongodb ValidatorError
-      handleValidatorError(err, res);
-      return;
-    }
-
-    res.status(err.statusCode /** Default err code is 500 */).json({
+    // Error is not a AppError Instance
+    // Send the Error status to the client
+    res.status(err.statusCode /** Default error code is 500 */).json({
       status: err.status /** Default error status is "error" */,
       message: "Something went wrong!",
     });

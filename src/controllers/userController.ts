5dc9ef9adcf930c551e376d7a5d13658967895f6
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-11 21:22:18 +0530

    removed unnecessary route handlers and added update-user route handler

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,19 +1,34 @@
 import { Response, Request } from "express";
 import User from "../models/User";
 import {
+  authorizeUsers,
   controller,
-  del,
   get,
   patch,
-  post,
-  put,
-  validateBody,
+  use,
+  useAsync,
 } from "./decorators";
-import { TypeStrings } from "./decorators/enums/typeStrings";
+import { UserTypes } from "./decorators/enums/userTypes";
+import { filterRequestBody, loginRequired } from "./middlewares";
+
+// function filterRequestBody(
+//   body: { [key: string]: any },
+//   ...fields: string[]
+// ): { [key: string]: any } {
+//   const newBodyObject: { [key: string]: any } = {};
+
+//   Object.keys(body).forEach((key: string) => {
+//     if (fields.includes(key)) newBodyObject[key] = body[key];
+//   });
+
+//   return newBodyObject;
+// }
 
 @controller("/api/v1/users")
 class UserController {
   @get("/")
+  @useAsync(loginRequired)
+  @authorizeUsers(UserTypes.admin)
   async getAllUsers(req: Request, res: Response): Promise<void> {
     try {
       const users = await User.find();
@@ -28,85 +43,27 @@ class UserController {
     }
   }
 
-  @post("/")
-  @validateBody(
-    { name: "name", type: TypeStrings.String },
-    { name: "age", type: TypeStrings.Number }
-  )
-  async addUser(req: Request, res: Response): Promise<void> {
-    try {
-      console.log("newUser");
-      // create new user in the db
-      const newUser = await User.create({
-        name: req.body.name,
-        age: req.body.age,
-      });
-
-      // if no exceptions are thrown, send success response
-      res.status(200).json({ status: "success", data: newUser });
-    } catch (error: any) {
-      res.status(503).json({ status: "failed", message: error.message });
-    }
-  }
-
-  @get("/:id")
-  async getUser(req: Request, res: Response): Promise<void> {
-    try {
-      const user = await User.findById(req.params.id);
-
-      res.status(200).json({ status: "success", data: user });
-    } catch (error: any) {
-      res.status(404).json({ status: "failed", message: "User not found!" });
-    }
-  }
-
-  @patch("/:id")
+  @patch("/update-user")
+  @useAsync(loginRequired)
+  @use(filterRequestBody("name", "username", "email"))
   async updateUser(req: Request, res: Response): Promise<void> {
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true, runValidators: true }
-      );
+    console.log(req.body);
 
-      res.status(200).json({ status: "success", data: updatedUser });
-    } catch (error: any) {
-      res.status(500).json({ status: "failed", message: error.message });
-    }
+    // get the current user from request object
+    const currentUser: UserDocument = (req as { [key: string]: any }).user;
 
-    res.send("User Updated successfully");
-  }
+    // update the user
+    const updatedUser = await User.findByIdAndUpdate(
+      currentUser._id,
+      req.body,
+      { new: true, runValidators: true }
+    );
 
-  @put("/:id")
-  @validateBody(
-    { name: "name", type: TypeStrings.String },
-    { name: "age", type: TypeStrings.Number }
-  )
-  async changeUser(req: Request, res: Response): Promise<void> {
-    try {
-      const changedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          name: req.body.name,
-          age: req.body.age,
-        },
-        { new: true, runValidators: true }
-      );
-
-      res.status(200).json({ status: "success", data: changedUser });
-    } catch (error: any) {
-      res.status(503).json({ status: "failed", message: error.message });
-    }
-  }
-
-  @del("/:id")
-  async deleteUser(req: Request, res: Response): Promise<void> {
-    try {
-      await User.findByIdAndDelete(req.params.id);
-
-      res.status(204).json({ status: "success", data: null });
-    } catch (error: any) {
-      res.status(503).json({ status: "failed", message: error.message });
-    }
+    // send the success response along with the updated user object
+    res.status(200).json({
+      status: "success",
+      message: "User updated successfully",
+      data: updatedUser,
+    });
   }
 }
